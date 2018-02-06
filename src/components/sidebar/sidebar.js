e724a1c9260cfd45299e6585389c5b56db8686d9
Bill Kidwell <kidwell.bill@gmail.com>
2018-02-05 23:20:08 -0500

    class => className fix in sidebar
    This fixed my font awesome issue and a number of other layout issues.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,30 +5,30 @@ const Sidebar = ({ location }) => {
     return (
         <section id="sidebar">
             <section id="intro">
-                <a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
+                <a href="#" className="logo"><img src="images/logo.jpg" alt="" /></a>
                 <header>
                     <h2>Gatsby Starter Blog</h2>
                     <p>Another fine responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
                 </header>
             </section>
             
-            <section class="blurb">
+            <section className="blurb">
                 <h2>About</h2>
                 <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
-                <ul class="actions">
-                    <li><a href="#" class="button">Learn More</a></li>
+                <ul className="actions">
+                    <li><a href="#" className="button">Learn More</a></li>
                 </ul>
             </section>
 
             <section id="footer">
-                <ul class="icons">
-                    <li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
-                    <li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
-                    <li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
-                    <li><a href="#" class="fa-rss"><span class="label">RSS</span></a></li>
-                    <li><a href="#" class="fa-envelope"><span class="label">Email</span></a></li>
+                <ul className="icons">
+                    <li><a href="#" className="fa-twitter"><span className="label">Twitter</span></a></li>
+                    <li><a href="#" className="fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="#" className="fa-instagram"><span className="label">Instagram</span></a></li>
+                    <li><a href="#" className="fa-rss"><span className="label">RSS</span></a></li>
+                    <li><a href="#" className="fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
-                <p class="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+                <p className="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.</p>
             </section>
 
         </section>
